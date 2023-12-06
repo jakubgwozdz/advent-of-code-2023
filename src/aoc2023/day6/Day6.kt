e038810b38d01e9bc9c35c4867c1ccf816ac9e5e
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-06 07:44:23 +0100

    day 6 less naive

--- src/aoc2023/day6/Day6.kt
+++ src/aoc2023/day6/Day6.kt
@@ -2,10 +2,10 @@ package aoc2023.day6
 
 import aoc2023.Puzzle
 import aoc2023.getDay
-import aoc2023.ints
 import aoc2023.logged
 import aoc2023.longs
 import aoc2023.readAndParse
+import kotlin.math.sqrt
 
 fun main() {
     val input = readAndParse("local/${getDay {}}_input.txt", ::parse)
@@ -20,20 +20,22 @@ fun parse(inputStr: String) = inputStr.lines()
     .map { it.substringAfter(":").trim() }.let { (t, d) -> Input(t, d) }
     .logged()
 
-fun calc(t: Long, d: Long): Long {
-    var r = 0L
-    var i = 0L
-    while (i++<t) if (((t - i) * i) > d ) r++
-    return r
+fun calc(t: Long, d: Long): Long = t - 2 * firstBetter(t, d) + 1
+
+private fun firstBetter(t: Long, d: Long): Long {
+    var i = d/t
+    while ((t - i) * i <= d) i++
+    return i
 }
 
-fun part1(input: Input) =
-    input.time.longs().zip(input.distance.longs())
-        .map { (t, d) -> calc(t, d) }.fold(1L, Long::times)
-
-fun part2(input: Input) =
+private fun part1Races(input: Input) = input.time.longs().zip(input.distance.longs())
+private fun part2Races(input: Input) =
     input.time.filterNot { it.isWhitespace() }.longs().zip(input.distance.filterNot { it.isWhitespace() }.longs())
-        .map { (t, d) -> calc(t, d) }.fold(1L, Long::times)
+
+fun part1(input: Input) = part1Races(input).map { (t, d) -> calc(t, d) }.reduce(Long::times)
+
+fun part2(input: Input) = part2Races(input).map { (t, d) -> calc(t, d) }.reduce(Long::times)
+
 
 
 
