259f25f17667c7df5bd20c820de3c7f58c6c1362
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-06 08:42:26 +0100

    day 6 integer sqrt

--- src/aoc2023/day6/Day6.kt
+++ src/aoc2023/day6/Day6.kt
@@ -5,8 +5,6 @@ import aoc2023.getDay
 import aoc2023.logged
 import aoc2023.longs
 import aoc2023.readAndParse
-import kotlin.math.ceil
-import kotlin.math.sqrt
 
 fun main() {
     val input = readAndParse("local/${getDay {}}_input.txt", ::parse)
@@ -19,13 +17,30 @@ data class Input(val time: String, val distance: String)
 
 fun parse(inputStr: String) = inputStr.lines()
     .map { it.substringAfter(":").trim() }.let { (t, d) -> Input(t, d) }
-    .logged()
+//    .logged()
 
 fun calc(t: Long, d: Long): Long = t - 2 * firstBetter(t, d) + 1
 
 fun firstBetter(t: Long, d: Long) = (t - (t * t - 4 * d).ceilSqrt()) / 2 + 1
 
-fun Long.ceilSqrt() = ceil(sqrt(toDouble())).toLong()
+// inspired by pseudocode at https://rosettacode.org/wiki/Isqrt_(integer_square_root)_of_X
+fun Long.ceilSqrt(): Long {
+    require(this >= 0)
+    var q = 1L
+    while (q <= this) q = q shl 2
+    var z = this
+    var r = 0L
+    while (q > 1L) {
+        q = q shr 2
+        val t = z - r - q
+        r = r shr 1
+        if (t >= 0) {
+            z = t
+            r += q
+        }
+    }
+    return if (z == 0L) r else r + 1
+}
 
 private fun part1Races(input: Input) = input.time.longs().zip(input.distance.longs())
 private fun part2Races(input: Input) =
