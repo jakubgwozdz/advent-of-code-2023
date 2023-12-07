0d3bd2a09806e5729526b27e9d1fcd992f7d92fb
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-07 12:06:13 +0100

    day 7 final refactor

--- src/aoc2023/day7/Day7.kt
+++ src/aoc2023/day7/Day7.kt
@@ -15,33 +15,31 @@ fun parse(inputStr: String) = inputStr.lines().filterNot { it.isBlank() }
     .map { it.split(" ") }
     .map { (a, b) -> a to b.toInt() }
 
-const val order = "_j23456789TJQKA"
+const val order = "_j23456789TJQKA_"
 
-data class Hand(val type: Int, val kinds: List<Int>) : Comparable<Hand> {
-    val value = kinds.fold(type) {acc, i -> acc * order.length + i }
+data class Hand(val type: Int, val cards: String) : Comparable<Hand> {
+    val value = cards.fold(type) { acc, c -> acc * 16 + order.indexOf(c) }
     override fun compareTo(other: Hand): Int = value.compareTo(other.value)
+    override fun toString() = value.toString(16).padStart(8,'0')
 }
 
 fun normalHand(cards: String): Hand {
     val counts = cards.groupBy { it }.map { (_, v) -> v.size }.sortedDescending()
-    val type = counts[0] * 10 + counts.getOrElse(1) { 0 }
-    val kinds = cards.map(order::indexOf)
-    return Hand(type, kinds)
+    val type = (counts[0] * 16 + counts.getOrElse(1) { 0 })*16
+    return Hand(type, cards)
 }
 
 fun jokeHand(cards: String): Hand {
     if ('J' !in cards) return normalHand(cards)
     val bestType = "234567890TQKA".maxOf { normalHand(cards.replace('J', it)) }.type
-    val kinds = cards.replace('J', 'j').map(order::indexOf)
-    return Hand(bestType, kinds)
+    return Hand(bestType, cards.replace('J', 'j'))
 }
 
 typealias Input = List<Pair<String, Int>>
 
 fun Input.solve(op: (String) -> Hand) = map { op(it.first) to it.second }
     .sortedBy { it.first }
-    .map { it.second }
-    .mapIndexed { index, bid -> (index + 1) * bid }
+    .mapIndexed { index, (_, bid) -> (index + 1) * bid }
     .sum()
 
 fun part1(input: Input) = input.solve(::normalHand)
