8a43bd2611a96de6724f936c9de6ab8b71370903
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-10 09:20:25 +0100

    day 10 part2 cheat

--- src/aoc2023/day10/Day10.kt
+++ src/aoc2023/day10/Day10.kt
@@ -1,9 +1,12 @@
 package aoc2023.day10
 
 import aoc2023.Puzzle
+import aoc2023.day10.Move.*
 import aoc2023.getDay
 import aoc2023.logged
 import aoc2023.readAndParse
+import java.util.Comparator
+import kotlin.math.absoluteValue
 
 fun main() {
     val input = readAndParse("local/${getDay {}}_input.txt", ::parse)
@@ -12,41 +15,84 @@ fun main() {
     puzzle.part2()
 }
 
-//data class Input(val todo: Int)
-typealias Input = List<String>
-
-fun parse(inputStr: String): Input {
-    return inputStr.lines().filterNot { it.isBlank() }
-}
-
 typealias Pos = Pair<Int, Int>
 
+data class Input(val map: List<String>, val start: Pos)
+
+fun parse(inputStr: String, cheat: Char = '|'): Input {
+    val lines = inputStr.lines()
+    val start = lines.indexOfFirst { it.contains('S') }.let { r -> r to lines[r].indexOf('S') }
+    return Input(lines.map { it.replace('S', cheat) }, start)
+}
+
 fun findBoth(pos: Pos, input: Input): List<Pos> = pos.let { (r, c) ->
-    when (input[r][c]) {
-        'S' -> listOf(r + 1 to c, r - 1 to c)
+    when (input.map[r][c]) {
+//        'S' -> listOf(r + 1 to c, r - 1 to c)
         '|' -> listOf(r + 1 to c, r - 1 to c)
         '-' -> listOf(r to c - 1, r to c + 1)
         'L' -> listOf(r - 1 to c, r to c + 1)
         'J' -> listOf(r - 1 to c, r to c - 1)
         'F' -> listOf(r + 1 to c, r to c + 1)
         '7' -> listOf(r + 1 to c, r to c - 1)
-        else -> error("${input[r][c]} at $pos")
+        else -> error("${input.map[r][c]} at $pos")
     }
 }
 
-fun part1(input: Input): Int {
-    val start:Pos = input.indexOfFirst { it.contains('S') }.let { r -> r to input[r].indexOf('S') }
-        .logged { "Pos" }
-    val last = generateSequence((start to start) to 0) { (pos2, steps) ->
-        val (prevPos, pos) = pos2
-        val newPos = findBoth(pos, input).first { it != prevPos }
+fun part1(input: Input): Int = // 7086
+    input.loop().count() / 2
+
+private fun Input.loop(): Sequence<Pos> {
+    val loop = generateSequence(start to start) { (prevPos, pos) ->
+        val newPos = findBoth(pos, this).first { it != prevPos }
         if (newPos == start) null
-        else ((pos to newPos) to steps + 1).logged()
-    }.last()
-        .logged()
-    return (last.second + 1) / 2
+        else pos to newPos
+    }
+    return loop.map { it.second }
 }
 
-fun part2(input: Input): Any? = null
+enum class Move { N, S, W, E }
+
+fun List<Move>.countInsides(): Int {
+    var sum = 0
+    var dx = 0
+    forEach { move ->
+        when (move) {
+            N -> sum -= dx
+            S -> sum += dx
+            W -> dx--
+            E -> dx++
+        }
+    }
+    return sum.absoluteValue - (size/2)+1
+}
+
+fun part2(input: Input): Int {
+    val loop = input.loop().toList()
+    val moves = (loop + loop.first()).zipWithNext(::move)
+    return moves.countInsides()
+}
+
+private fun List<Pos>.normalize(): List<Pos> {
+    val topLeft = minWith(Comparator.comparingInt(Pos::first).thenComparingInt(Pos::second))
+    val index = indexOf(topLeft)
+    val indexOfNext = (index + 1) % size
+    return when (move(this[index], this[indexOfNext])) {
+        E -> drop(index) + take(index)
+        S -> (drop(index) + take(index)).reversed().let { listOf(it.last()) + it.dropLast(1) }
+        else -> error("wtf")
+    }
+}
+
+private fun move(pos1: Pos, pos2: Pos): Move {
+    val (r1, c1) = pos1
+    val (r2, c2) = pos2
+    return when (r2 - r1 to c2 - c1) {
+        -1 to 0 -> N
+        1 to 0 -> S
+        0 to -1 -> W
+        0 to 1 -> E
+        else -> error("$pos1 moves to $pos2?")
+    }
+}
 
 
--- test/aoc2023/day10/Day10Test.kt
+++ test/aoc2023/day10/Day10Test.kt
@@ -1,38 +1,89 @@
 package aoc2023.day10
 
+import aoc2023.day10.Move.E
+import aoc2023.day10.Move.N
+import aoc2023.day10.Move.S
+import aoc2023.day10.Move.W
 import org.junit.jupiter.api.Timeout
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.expect
 
 class Day10Test {
 
-    val testInput: Input? = null
-
-    val testInputStr = """
-        42
-    """.trimIndent()
-
-
     @Test
-    @Timeout(1)
-    @Ignore
-    fun parseTest() {
-        expect(testInput) { parse(testInputStr) }
+    fun part2t0a() {
+        expect(16) { listOf(E, E, E, E, E, S, S, S, S, S, W, W, W, W, W, N, N, N, N, N).countInsides() }
     }
 
     @Test
     @Timeout(5)
-    @Ignore
-    fun part1test() {
-        expect(226) { part1(parse(testInputStr)) }
+    fun part2test1() {
+        val testInputStr = """
+            ...........
+            .S-------7.
+            .|F-----7|.
+            .||.....||.
+            .||.....||.
+            .|L-7.F-J|.
+            .|..|.|..|.
+            .L--J.L--J.
+            ...........
+        """.trimIndent()
+        expect(4) { part2(parse(testInputStr, 'F')) }
     }
 
     @Test
     @Timeout(5)
-    @Ignore
-    fun part2test() {
-        expect(3) { part2(parse(testInputStr)) }
+    fun part2test1a() {
+        val testInputStr = """
+            ..........
+            .S------7.
+            .|F----7|.
+            .||....||.
+            .||....||.
+            .|L-7F-J|.
+            .|..||..|.
+            .L--JL--J.
+            ..........
+        """.trimIndent()
+        expect(4) { part2(parse(testInputStr, 'F')) }
     }
+
+    @Test
+    @Timeout(5)
+    fun part2test2() {
+        val testInputStr = """
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+        """.trimIndent()
+        expect(8) { part2(parse(testInputStr, 'F')) }
+    }
+
+    @Test
+    @Timeout(5)
+    fun part2test3() {
+        val testInputStr = """
+            FF7FSF7F7F7F7F7F---7
+            L|LJ||||||||||||F--J
+            FL-7LJLJ||||||LJL-77
+            F--JF--7||LJLJ7F7FJ-
+            L---JF-JLJ.||-FJLJJ7
+            |F|F-JF---7F7-L7L|7|
+            |FFJF7L7F-JF7|JL---7
+            7-L-JL7||F7|L7F-7F7|
+            L.L7LFJ|||||FJL7||LJ
+            L7JLJL-JLJLJL--JLJ.L
+        """.trimIndent()
+        expect(10) { part2(parse(testInputStr, '7')) }
+    }
+
 
 }
