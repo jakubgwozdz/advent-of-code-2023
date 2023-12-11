77d543bbcf8885efb8fb77c3d11fab1b1fc2ea19
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-11 14:34:57 +0100

    day 11

--- src/aoc2023/day11/Day11.kt
+++ src/aoc2023/day11/Day11.kt
@@ -26,11 +26,14 @@ fun part2(input: Input) = input.calc2D(1000000)
 
 fun Input.calc2D(expansion: Int) = map { it.first }.calc1D(expansion) + map { it.second }.calc1D(expansion)
 
-private fun List<Int>.calc1D(expansion: Int) = expanded(expansion).distances()
+fun List<Int>.calc1D(expansion: Int) = expanded(expansion).distinctWithCounts().distances()
 
-private fun List<Long>.distances() = sumOf { r1 -> sumOf { r2 -> (r2 - r1).absoluteValue } } / 2
-
-private fun List<Int>.expanded(expansion: Int): List<Long> = sorted()
+fun List<Int>.expanded(expansion: Int): List<Long> = sorted()
     .zipWithNext { a, b -> (b - a).toLong() }
     .map { if (it > 1) (it - 1) * expansion + 1 else it }
     .runningFold(first().toLong(), Long::plus)
+
+fun List<Long>.distinctWithCounts() = groupBy { it }.map { it.key to it.value.size }
+
+fun List<Pair<Long, Int>>.distances() =
+    sumOf { (v1, c1) -> sumOf { (v2, c2) -> (v2 - v1).absoluteValue * c1 * c2 } } / 2
