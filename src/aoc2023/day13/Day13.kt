8edf753d41dac32fab41fb65d5d6e90f42dd0380
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-13 07:47:10 +0100

    day 13 loops go brrr obscene amount of times

--- src/aoc2023/day13/Day13.kt
+++ src/aoc2023/day13/Day13.kt
@@ -11,18 +11,43 @@ fun main() {
     puzzle.part2()
 }
 
-//data class Input(val todo: Int)
-typealias Input = List<String>
+typealias Mirror = List<String>
+typealias Input = List<Mirror>
 
-fun parse(inputStr: String): Input {
-    return inputStr.lines().filterNot { it.isBlank() }
-//    return Input("Damage: (\\d+)".toRegex().find(inputStr)?.destructured?.component1()?.toInt()!!)
-//    TODO("parse ${inputStr.length} data")
+fun parse(inputStr: String): Input = inputStr.split("\n\n").map { it.lines().filterNot(String::isBlank) }
+    .filterNot { it.isEmpty() }
 
+fun findVertical(mirror: Mirror, ignore: Int): Int? = findHorizontal(mirror.transposed(), ignore)
+
+private fun Mirror.transposed(): Mirror = (0..<first().length)
+    .map { c -> buildString { this@transposed.forEach { row -> append(row[c]) } } }
+
+fun findHorizontal(mirror: Mirror, ignore: Int) = (1..mirror.lastIndex)
+    .filterNot { it == ignore }
+    .firstOrNull {
+        val before = mirror.take(it).reversed()
+        val after = mirror.drop(it)
+        val lines = before.size.coerceAtMost(after.size)
+        before.take(lines) == after.take(lines)
+    }
+
+fun calc(mirror: Mirror, ignore: Int = 0) = findVertical(mirror, ignore % 100)
+    ?: findHorizontal(mirror, ignore / 100)?.let { it * 100 }
+
+fun part1(input: Input) = input.sumOf { calc(it) ?: error("no reflection") } // 37718
+
+fun Char.flip() = when (this) {
+    '#' -> '.'
+    '.' -> '#'
+    else -> error("$this?")
 }
 
-fun part1(input: Input): Any? = input.toString() // TODO("part 1 with ${input.toString().length} data")
+private fun Mirror.flip(r: Int, c: Int): Mirror =
+    take(r) + this[r].let { it.take(c) + it[c].flip() + it.drop(c + 1) } + drop(r + 1)
 
-fun part2(input: Input): Any? = null // TODO("part 2 with ${input.toString().length} data")
-
-
+fun part2(input: Input) = input.sumOf { mirror -> //40995
+    val original = calc(mirror)!!
+    mirror
+        .indices.asSequence().flatMap { r -> mirror[r].indices.asSequence().map { c -> r to c } }
+        .firstNotNullOf { (r, c) -> calc(mirror.flip(r, c), original) }
+}
--- test/aoc2023/day13/Day13Test.kt
+++ test/aoc2023/day13/Day13Test.kt
@@ -9,7 +9,23 @@ class Day13Test {
 
     val testInput: Input? = null
 
-    val testInputStr = """""".trimIndent()
+    val testInputStr = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+    """.trimIndent()
 
 
     @Test
@@ -21,16 +37,14 @@ class Day13Test {
 
     @Test
     @Timeout(5)
-    @Ignore
     fun part1test() {
-        expect(226) { part1(parse(testInputStr)) }
+        expect(405) { part1(parse(testInputStr)) }
     }
 
     @Test
     @Timeout(5)
-    @Ignore
     fun part2test() {
-        expect(3) { part2(parse(testInputStr)) }
+        expect(400) { part2(parse(testInputStr)) }
     }
 
 }
