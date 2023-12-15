92fb326c39280be8d23e54c58d0226f2bf4dbe98
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-15 07:52:21 +0100

    main changed

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,6 @@
-fun main() {
-    aoc2023.day1.main()
-    aoc2023.day2.main()
-    aoc2023.day3.main()
-    aoc2023.day4.main()
-    aoc2023.day5.main()
-    aoc2023.day6.main()
-    aoc2023.day7.main()
-    aoc2023.day8.main()
-    aoc2023.day9.main()
-    aoc2023.day10.main()
-    aoc2023.day11.main()
-    aoc2023.day12.main()
-    aoc2023.day13.main()
-    aoc2023.day14.main()
-    aoc2023.day15.main()
+fun main() = (1..25).forEach {
+    try {
+        Class.forName("aoc2023.day$it.Day${it}Kt").getMethod("main").invoke(null)
+    } catch (_: ClassNotFoundException) {
+    }
 }
