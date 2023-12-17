46dfe4a566c9c7aee29eca2b283591f7acd7e399
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-18 00:16:38 +0100

    day 17 part 2

--- src/aoc2023/day17/Day17.kt
+++ src/aoc2023/day17/Day17.kt
@@ -52,52 +52,45 @@ operator fun Dir.unaryMinus() = when (this) {
 operator fun Input.contains(p: Pos) = p.r in this.indices && p.c in this[p.r].indices
 private operator fun Input.get(p: Pos) = this[p.r][p.c].digitToInt()
 
-typealias Key = Pair<Pos, Dir?>
+typealias Key = Pair<Pos, Boolean?>
 
 data class State(val pos: Pos, val lastMove: Dir?, val cost: Int) : Comparable<State> {
     override fun compareTo(other: State): Int = this.cost.compareTo(other.cost)
-    val key: Key = pos to lastMove
+    val key: Key = pos to lastMove?.let { it == E || it == W }
 }
 
 fun part1(input: Input) = input.solve(1, 3)
 fun part2(input: Input) = input.solve(4, 10)
 
 fun Input.solve(minMoves: Int, maxMoves: Int): Int? {
-    val queue = PriorityQueue(compareBy<State> { it.cost })
-    val isEnd: (State) -> Boolean = { it.pos.r == lastIndex && it.pos.c == last().lastIndex }
-    val moves: (State) -> List<State> = { s ->
-        buildList {
-            val dirs = when (s.lastMove) {
-                E, W -> listOf(S, N)
-                N, S -> listOf(E, W)
-                null -> listOf(S, E)
-            }
-            dirs.forEach { dir ->
-                var nextPos = s.pos + dir
-                var i = 0
-                var nextCost = s.cost
-                while ((i < maxMoves) && (nextPos in this@solve)) {
-                    i++
-                    nextCost += this@solve[nextPos]
-                    if (i >= minMoves) add(State(nextPos, dir, nextCost))
-                    nextPos += dir
-                }
+    val besties = mutableMapOf<Key, Int>()
+    fun testChecking(s: State) = besties[s.key].let { b -> b == null || b > s.cost }
+    fun moves(s: State) = buildList {
+        val dirs = when (s.lastMove) {
+            E, W -> listOf(S, N)
+            N, S -> listOf(E, W)
+            null -> listOf(S, E)
+        }
+        dirs.forEach { dir ->
+            var nextPos = s.pos + dir
+            var i = 0
+            var nextCost = s.cost
+            while ((i < maxMoves) && (nextPos in this@solve)) {
+                i++
+                nextCost += this@solve[nextPos]
+                if (i >= minMoves) add(State(nextPos, dir, nextCost))
+                nextPos += dir
             }
         }
-    }
-    val besties = mutableMapOf<Key, Int>()
-    val isBetter: (State) -> Boolean = {
-        besties[it.key]
-            .let { b -> b == null || b > it.cost }
-            .also { better -> if (better) besties[it.key] = it.cost }
-    }
+    }.filter(::testChecking)
+
     return search(
         State(Pos(0, 0), null, 0),
-        moves,
-        { it.cost },
-        isBetter,
-        isEnd,
-        queue,
+        ::moves,
+        State::cost,
+        { testChecking(it).also { better -> if (better) besties[it.key] = it.cost } },
+        { it.pos.r == lastIndex && it.pos.c == last().lastIndex },
+        PriorityQueue(compareBy { it.cost }),
     )
 }
 
@@ -105,7 +98,7 @@ fun <T : Any, R : Comparable<R>> search(
     startState: T,
     moves: (T) -> List<T>,
     valueOp: (T) -> R,
-    worth: (T) -> Boolean,
+    isWorthChecking: (T) -> Boolean,
     isEnd: (T) -> Boolean,
     queue: Queue<T> = Queue(),
 ): R? {
@@ -114,7 +107,7 @@ fun <T : Any, R : Comparable<R>> search(
     while (toGo.isNotEmpty()) {
         val next = toGo.poll()
         val value = valueOp(next)
-        if (worth(next) && (min == null || value < min)) {
+        if (isWorthChecking(next) && (min == null || value < min)) {
             if (isEnd(next)) min = value else moves(next).forEach(toGo::offer)
         }
     }
