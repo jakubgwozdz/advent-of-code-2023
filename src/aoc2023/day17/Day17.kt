affdd5273fd4c4b32fc7506ef714bb61ea79f660
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-17 20:14:19 +0100

    day 17 part 2

--- src/aoc2023/day17/Day17.kt
+++ src/aoc2023/day17/Day17.kt
@@ -2,13 +2,13 @@ package aoc2023.day17
 
 import aoc2023.PriorityQueue
 import aoc2023.Puzzle
+import aoc2023.Queue
 import aoc2023.day17.Dir.E
 import aoc2023.day17.Dir.N
 import aoc2023.day17.Dir.S
 import aoc2023.day17.Dir.W
 import aoc2023.expect
 import aoc2023.getDay
-import aoc2023.logged
 import aoc2023.readAndParse
 
 fun main() {
@@ -20,9 +20,7 @@ fun main() {
 
 typealias Input = List<String>
 
-fun parse(inputStr: String): Input {
-    return inputStr.lines().filterNot { it.isBlank() }
-}
+fun parse(inputStr: String): Input = inputStr.lines().filterNot { it.isBlank() }
 
 enum class Dir { N, E, S, W }
 data class Pos(val r: Int, val c: Int)
@@ -52,87 +50,90 @@ operator fun Dir.unaryMinus() = when (this) {
 data class State(val pos: Pos, val lastMoves: Pair<Dir, Int>)
 data class StateWithData(val state: State, val visited: Set<Pos>, val cost: Int)
 
-fun part1(input: Input): Any {
+fun part1(input: Input) = input.solve(1, 3)
+fun part2(input: Input) = input.solve(4, 10)
 
-    fun StateWithData.possibleMoves(): List<StateWithData> = buildList {
-        val (lastDir, count) = state.lastMoves
-        val back = -lastDir
-        val last3dir = if (count >= 3) lastDir else null
+fun Input.solve(minMoves: Int, maxMoves: Int): Int? {
+    val queue =
+        PriorityQueue<StateWithData>(compareBy<StateWithData> { it.cost + (it.state.pos.r + it.state.pos.c) * 1 })
+    val isEnd: (StateWithData) -> Boolean = { it.state.pos.r == lastIndex && it.state.pos.c == last().lastIndex }
+    val moves: (StateWithData) -> List<StateWithData> = { s ->
+        if (s.state.lastMoves.second in (1..<minMoves)) {
+            buildList {
+                val (lastDir, count) = s.state.lastMoves
+                val newPos = s.state.pos + lastDir
+                if (newPos.r in this@solve.indices && newPos.c in this@solve[newPos.r].indices) {
+                    val newLastMoves = lastDir to count + 1
+                    val newState = State(newPos, newLastMoves)
+                    add(
+                        StateWithData(
+                            newState,
+                            s.visited + newPos,
+                            s.cost + this@solve[newPos.r][newPos.c].digitToInt()
+                        )
+                    )
+                }
+            }
+        } else buildList {
+            val (lastDir, count) = s.state.lastMoves
+            val back = -lastDir
+            val last3dir = if (count >= maxMoves) lastDir else null
 
-        Dir.entries.forEach { dir ->
-            val newPos = state.pos + dir
-            if (dir != back && dir != last3dir &&
-                newPos !in visited &&
-                newPos.r in input.indices && newPos.c in input[newPos.r].indices
-            ) {
-                val newLastMoves = dir to if (dir == lastDir) count + 1 else 1
-                val newState = State(newPos, newLastMoves)
-                add(StateWithData(newState, visited + newPos, cost + input[newPos.r][newPos.c].digitToInt()))
+            Dir.entries.forEach { dir ->
+                val newPos = s.state.pos + dir
+                if (dir != back && dir != last3dir &&
+                    newPos !in s.visited &&
+                    newPos.r in this@solve.indices && newPos.c in this@solve[newPos.r].indices
+                ) {
+                    val newLastMoves = dir to if (dir == lastDir) count + 1 else 1
+                    val newState = State(newPos, newLastMoves)
+                    add(
+                        StateWithData(
+                            newState,
+                            s.visited + newPos,
+                            s.cost + this@solve[newPos.r][newPos.c].digitToInt()
+                        )
+                    )
+                }
             }
         }
     }
-
-    val start = StateWithData(State(Pos(0, 0), E to 0), emptySet(), 0)
-    val end = Pos(input.lastIndex, input.last().lastIndex)
-    val toGo = PriorityQueue<StateWithData> { s1, s2 -> s1.cost - s2.cost }
-        .apply { offer(start) }
     val besties = mutableMapOf<State, Int>()
-    var min = Int.MAX_VALUE
-    while (toGo.isNotEmpty()) {
-        val stateWithCost = toGo.poll()
-        if ((besties[stateWithCost.state] ?: Int.MAX_VALUE) > stateWithCost.cost) {
-            besties[stateWithCost.state] = stateWithCost.cost
-            if (stateWithCost.state.pos == end) min =
-                min.coerceAtMost(stateWithCost.cost.logged(stateWithCost.state.lastMoves))
-            stateWithCost.possibleMoves().forEach(toGo::offer)
-        }
+    val isBetter: (StateWithData) -> Boolean = {
+        besties[it.state]
+            .let { b -> b == null || b > it.cost }
+            .also { better -> if (better) besties[it.state] = it.cost }
     }
-    return min
+
+    return search(
+        StateWithData(State(Pos(0, 0), E to 0), emptySet(), 0),
+        moves,
+        { it.cost },
+        isBetter,
+        isEnd,
+        queue,
+    )
 }
 
-fun part2(input: Input): Any {
-
-    fun StateWithData.possibleMoves(): List<StateWithData> = if (state.lastMoves.second in (1..3)) {
-        buildList {
-            val (lastDir, count) = state.lastMoves
-            val newPos = state.pos + lastDir
-            if (newPos.r in input.indices && newPos.c in input[newPos.r].indices) {
-                val newLastMoves = lastDir to count + 1
-                val newState = State(newPos, newLastMoves)
-                add(StateWithData(newState, visited + newPos, cost + input[newPos.r][newPos.c].digitToInt()))
-            }
-        }
-    } else buildList {
-        val (lastDir, count) = state.lastMoves
-        val back = -lastDir
-        val last3dir = if (count >= 10) lastDir else null
-
-        Dir.entries.forEach { dir ->
-            val newPos = state.pos + dir
-            if (dir != back && dir != last3dir &&
-                newPos !in visited &&
-                newPos.r in input.indices && newPos.c in input[newPos.r].indices
-            ) {
-                val newLastMoves = dir to if (dir == lastDir) count + 1 else 1
-                val newState = State(newPos, newLastMoves)
-                add(StateWithData(newState, visited + newPos, cost + input[newPos.r][newPos.c].digitToInt()))
-            }
-        }
-    }
-
-    val start = StateWithData(State(Pos(0, 0), E to 0), emptySet(), 0)
-    val end = Pos(input.lastIndex, input.last().lastIndex)
-    val toGo = PriorityQueue<StateWithData> { s1, s2 -> s1.cost - s2.cost }
-        .apply { offer(start) }
-    val besties = mutableMapOf<State, Int>()
-    var min = Int.MAX_VALUE
+fun <T : Any, R : Comparable<R>> search(
+    startState: T,
+    moves: (T) -> List<T>,
+    valueOp: (T) -> R,
+    worth: (T) -> Boolean,
+    isEnd: (T) -> Boolean,
+    queue: Queue<T> = Queue(),
+): R? {
+    val toGo = queue.apply { offer(startState) }
+    var min: R? = null
     while (toGo.isNotEmpty()) {
-        val stateWithCost = toGo.poll()
-        if ((besties[stateWithCost.state] ?: Int.MAX_VALUE) > stateWithCost.cost) {
-            besties[stateWithCost.state] = stateWithCost.cost
-            if (stateWithCost.state.pos == end) min =
-                min.coerceAtMost(stateWithCost.cost.logged(stateWithCost.state.lastMoves))
-            stateWithCost.possibleMoves().forEach(toGo::offer)
+        val next = toGo.poll()
+        val value = valueOp(next)
+        if (worth(next)) {
+            if (isEnd(next)) {
+                if (min == null || value < min) min = value
+            } else {
+                moves(next).forEach(toGo::offer)
+            }
         }
     }
     return min
