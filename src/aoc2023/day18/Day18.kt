1acaee85890328389339c00c616558e4ef0252a0
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-18 07:13:33 +0100

    day 18

--- src/aoc2023/day18/Day18.kt
+++ src/aoc2023/day18/Day18.kt
@@ -1,28 +1,49 @@
 package aoc2023.day18
 
 import aoc2023.Puzzle
+import aoc2023.day18.Dir.D
+import aoc2023.day18.Dir.L
+import aoc2023.day18.Dir.R
+import aoc2023.day18.Dir.U
+import aoc2023.expect
 import aoc2023.getDay
 import aoc2023.readAndParse
+import kotlin.math.absoluteValue
 
 fun main() {
     val input = readAndParse("local/${getDay {}}_input.txt", ::parse)
     val puzzle = Puzzle(input, ::part1, ::part2)
-    puzzle.part1()
-    puzzle.part2()
+    puzzle.part1().expect(50746L)
+    puzzle.part2().expect(70086216556038)
 }
 
-//data class Input(val todo: Int)
-typealias Input = List<String>
+enum class Dir { R, D, L, U }
 
-fun parse(inputStr: String): Input {
-    return inputStr.lines().filterNot { it.isBlank() }
-//    return Input("Damage: (\\d+)".toRegex().find(inputStr)?.destructured?.component1()?.toInt()!!)
-//    TODO("parse ${inputStr.length} data")
+data class Entry(val dir: Dir, val times: Int, val color: Int)
+typealias Input = List<Entry>
 
+val regex = "([LRDU]) (\\d+) \\(#(.{6})\\)".toRegex()
+fun parse(inputStr: String): Input = inputStr.lines()
+    .mapNotNull { regex.matchEntire(it) }
+    .map { it.destructured }
+    .map { (d, c, color) -> Entry(Dir.valueOf(d), c.toInt(), color.toInt(16)) }
+
+
+fun part1(input: Input) = calc(input.map { it.dir to it.times })
+fun part2(input: Input) = calc(input.map { (_, _, c) -> Dir.entries[c % 16] to c / 16 })
+
+fun calc(data: List<Pair<Dir, Int>>): Long {
+    var area = 0L
+    var outline = 0L
+    var y = 0L
+    data.forEach { (d, t) ->
+        when (d) {
+            R -> area += y * t
+            D -> y += t
+            L -> area -= y * t
+            U -> y -= t
+        }
+        outline += t
+    }
+    return area.absoluteValue + outline / 2 + 1
 }
-
-fun part1(input: Input): Any? = input.toString() // TODO("part 1 with ${input.toString().length} data")
-
-fun part2(input: Input): Any? = null // TODO("part 2 with ${input.toString().length} data")
-
-
--- test/aoc2023/day18/Day18Test.kt
+++ test/aoc2023/day18/Day18Test.kt
@@ -1,7 +1,6 @@
 package aoc2023.day18
 
 import org.junit.jupiter.api.Timeout
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.expect
 
@@ -10,29 +9,32 @@ class Day18Test {
     val testInput: Input? = null
 
     val testInputStr = """
-        42
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
     """.trimIndent()
 
-
-    @Test
-    @Timeout(1)
-    @Ignore
-    fun parseTest() {
-        expect(testInput) { parse(testInputStr) }
-    }
-
     @Test
     @Timeout(5)
-    @Ignore
     fun part1test() {
-        expect(226) { part1(parse(testInputStr)) }
+        expect(62L) { part1(parse(testInputStr)) }
     }
 
     @Test
     @Timeout(5)
-    @Ignore
     fun part2test() {
-        expect(3) { part2(parse(testInputStr)) }
+        expect(952408144115L) { part2(parse(testInputStr)) }
     }
 
 }
