5e561f2302cda50bc7080936d8e7beeda1a3f92f
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-19 06:45:59 +0100

    day 19 part1

--- src/aoc2023/day19/Day19.kt
+++ src/aoc2023/day19/Day19.kt
@@ -11,18 +11,65 @@ fun main() {
     puzzle.part2()
 }
 
-//data class Input(val todo: Int)
-typealias Input = List<String>
-
-fun parse(inputStr: String): Input {
-    return inputStr.lines().filterNot { it.isBlank() }
-//    return Input("Damage: (\\d+)".toRegex().find(inputStr)?.destructured?.component1()?.toInt()!!)
-//    TODO("parse ${inputStr.length} data")
-
+fun interface Rule {
+    fun test(part: Part): String?
 }
 
-fun part1(input: Input): Any? = input.toString() // TODO("part 1 with ${input.toString().length} data")
+data class IfGt(val rating: Char, val value: Int, val target: String) : Rule {
+    override fun test(part: Part) = if (part[rating]!! > value) target else null
+}
 
-fun part2(input: Input): Any? = null // TODO("part 2 with ${input.toString().length} data")
+data class IfLt(val rating: Char, val value: Int, val target: String) : Rule {
+    override fun test(part: Part) = if (part[rating]!! < value) target else null
+}
+
+data class Else(val target: String) : Rule {
+    override fun test(part: Part) = target
+}
+
+typealias Workflow = List<Rule>
+
+fun Workflow.test(part: Part): String {
+    forEach { it.test(part)?.let { return it } }
+    error("fail on workflow $this")
+}
+
+typealias Part = Map<Char, Int>
+
+data class Input(val workflows: Map<String, Workflow>, val parts: List<Part>)
+
+fun parse(inputStr: String): Input {
+    val r1 = "(\\w+)\\{(.+)}".toRegex()
+    val lines = inputStr.lines()
+    val rules = lines.takeWhile(String::isNotBlank)
+        .mapNotNull(r1::matchEntire)
+        .map(MatchResult::destructured)
+        .associate { (key, desc) ->
+            key to desc.split(",").map {
+                val target = it.substringAfter(":")
+                val c = it.first()
+                if (it.contains("<")) IfLt(c, it.substringAfter("<").substringBefore(":").toInt(), target)
+                else if (it.contains(">")) IfGt(c, it.substringAfter(">").substringBefore(":").toInt(), target)
+                else Else(target)
+            }
+        }
+    val parts = lines
+        .dropWhile(String::isNotBlank)
+        .filter(String::isNotBlank)
+        .map { it.drop(1).dropLast(1) }
+        .map { it.split(",").associate { it.first() to it.substringAfter("=").toInt() } }
+    return Input(rules, parts)
+}
+
+fun part1(input: Input) = input.parts.filter { part ->
+    var w = "in"
+    while (w in input.workflows) w = input.workflows[w]!!.test(part)
+    w == "A"
+}.sumOf { part -> part.values.sum() }
+
+fun part2(input: Input): Long {
+
+    TODO()
+}
 
 
--- test/aoc2023/day19/Day19Test.kt
+++ test/aoc2023/day19/Day19Test.kt
@@ -10,7 +10,23 @@ class Day19Test {
     val testInput: Input? = null
 
     val testInputStr = """
-        42
+        px{a<2006:qkq,m>2090:A,rfg}
+        pv{a>1716:R,A}
+        lnx{m>1548:A,A}
+        rfg{s<537:gd,x>2440:R,A}
+        qs{s>3448:A,lnx}
+        qkq{x<1416:A,crn}
+        crn{x>2662:A,R}
+        in{s<1351:px,qqz}
+        qqz{s>2770:qs,m<1801:hdj,R}
+        gd{a>3333:R,R}
+        hdj{m>838:A,pv}
+
+        {x=787,m=2655,a=1222,s=2876}
+        {x=1679,m=44,a=2067,s=496}
+        {x=2036,m=264,a=79,s=2244}
+        {x=2461,m=1339,a=466,s=291}
+        {x=2127,m=1623,a=2188,s=1013}
     """.trimIndent()
 
 
@@ -23,16 +39,14 @@ class Day19Test {
 
     @Test
     @Timeout(5)
-    @Ignore
     fun part1test() {
-        expect(226) { part1(parse(testInputStr)) }
+        expect(19114) { part1(parse(testInputStr)) }
     }
 
     @Test
     @Timeout(5)
-    @Ignore
     fun part2test() {
-        expect(3) { part2(parse(testInputStr)) }
+        expect(167409079868000) { part2(parse(testInputStr)) }
     }
 
 }
