2ebf9a414968c48ba79a9db72e1db46883e798be
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-20 22:01:47 +0100

    pre day 21

---
+++ test/aoc2023/day21/Day21Test.kt
@@ -0,0 +1,38 @@
+package aoc2023.day21
+
+import org.junit.jupiter.api.Timeout
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.expect
+
+class Day21Test {
+
+    val testInput: Input? = null
+
+    val testInputStr = """
+        42
+    """.trimIndent()
+
+
+    @Test
+    @Timeout(1)
+    @Ignore
+    fun parseTest() {
+        expect(testInput) { parse(testInputStr) }
+    }
+
+    @Test
+    @Timeout(5)
+    @Ignore
+    fun part1test() {
+        expect(226) { part1(parse(testInputStr)) }
+    }
+
+    @Test
+    @Timeout(5)
+    @Ignore
+    fun part2test() {
+        expect(3) { part2(parse(testInputStr)) }
+    }
+
+}
