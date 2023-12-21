a9cf3fe4db2600a004d316dd48a01f6edcd032b4
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-21 07:28:20 +0100

    day 21 part 1

--- src/aoc2023/day21/Day21.kt
+++ src/aoc2023/day21/Day21.kt
@@ -36,12 +36,22 @@ fun part1(input: Input) = calc(input, 64)
 fun part2(input: Input) = calc(input, 26501365)
 
 fun calc(input: Input, steps: Int): Long {
+    val size = input.size
+    check(input.all { it.length == size })
     val initial: Pos = input.indexOfFirst { it.contains("S") }.let { r -> Pos(r, input[r].indexOf('S')) }
+    val array = Array(size) { BooleanArray(size) }
+    input.indices.forEach { r -> input[r].indices.forEach { c -> array[r][c] = input[r][c] != '#' } }
+
     var prev = setOf(initial)
     repeat(steps) {
         prev = prev.flatMap { p ->
-            Dir.entries.map { p + it }.filter { (r, c) -> input[r][c] == '.' }
+            Dir.entries.map { p + it }.filter { (r, c) -> array[r.mod(size)][c.mod(size)] }
         }.toSet() - prev
     }
-    return prev.size.toLong() + 1
+
+    array.indices.forEach { r ->
+        println(array[r].indices.map { c -> if (Pos(r, c) in prev) 'O' else if (array[r][c]) '.' else '#' }
+            .joinToString(""))
+    }
+    return prev.size.toLong()
 }
--- test/aoc2023/day21/Day21Test.kt
+++ test/aoc2023/day21/Day21Test.kt
@@ -1,9 +1,11 @@
 package aoc2023.day21
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
-import kotlin.test.Test
+import java.util.concurrent.TimeUnit
 import kotlin.test.expect
 
+@Timeout(1, unit = TimeUnit.SECONDS)
 class Day21Test {
 
     val testInput: Input? = null
@@ -24,16 +26,25 @@ class Day21Test {
 
     @Test
     @Timeout(5)
-    fun part1test() {
+    fun test6() {
         expect(16L) { calc(parse(testInputStr),6) }
     }
 
     @Test
     @Timeout(5)
-    fun part2test1() {
+    fun test10() {
         expect(50L) { calc(parse(testInputStr),10) }
+    }
+
+    @Test
+    @Timeout(5)
+    fun test50() {
         expect(1594L) { calc(parse(testInputStr),50) }
-        expect(16733044L) { calc(parse(testInputStr),5000) }
+    }
+
+    @Test
+    fun test5000() {
+//        expect(16733044L) { calc(parse(testInputStr),5000) }
     }
 
 }
