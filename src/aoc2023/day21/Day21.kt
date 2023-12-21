4ca61ab8465fb3414cf21761ae841bfaf5b4eb6c
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-21 20:45:55 +0100

    day 21 part 1

--- src/aoc2023/day21/Day21.kt
+++ src/aoc2023/day21/Day21.kt
@@ -1,12 +1,9 @@
 package aoc2023.day21
 
 import aoc2023.Puzzle
-import aoc2023.day21.Dir.E
-import aoc2023.day21.Dir.N
-import aoc2023.day21.Dir.S
-import aoc2023.day21.Dir.W
 import aoc2023.expect
 import aoc2023.getDay
+import aoc2023.logged
 import aoc2023.readAndParse
 
 fun main() {
@@ -22,36 +19,29 @@ fun parse(inputStr: String): Input {
     return inputStr.lines().filterNot { it.isBlank() }
 }
 
-enum class Dir { N, E, S, W }
-data class Pos(val r: Int, val c: Int)
+data class Pos(val r: Long, val c: Long)
 
-operator fun Pos.plus(d: Dir): Pos = when (d) {
-    N -> Pos(r - 1, c)
-    E -> Pos(r, c + 1)
-    S -> Pos(r + 1, c)
-    W -> Pos(r, c - 1)
-}
-
+operator fun Pos.plus(o: Pos) = Pos(r + o.r, c + o.c)
 fun part1(input: Input) = calc(input, 64)
-fun part2(input: Input) = calc(input, 26501365)
+
+//fun part2(input: Input) = calc(input, 2)
+fun part2(input: Input): Any = TODO()//calc(input, 26501365)
 
 fun calc(input: Input, steps: Int): Long {
+    input.withIndex().sumOf { (r, l) -> l.withIndex().count { (c, ch) -> c % 2 == r % 2 && ch != '#' } }.logged("even")
+    input.withIndex().sumOf { (r, l) -> l.withIndex().count { (c, ch) -> c % 2 != r % 2 && ch != '#' } }.logged("odd")
     val size = input.size
     check(input.all { it.length == size })
-    val initial: Pos = input.indexOfFirst { it.contains("S") }.let { r -> Pos(r, input[r].indexOf('S')) }
-    val array = Array(size) { BooleanArray(size) }
-    input.indices.forEach { r -> input[r].indices.forEach { c -> array[r][c] = input[r][c] != '#' } }
-
-    var prev = setOf(initial)
-    repeat(steps) {
-        prev = prev.flatMap { p ->
-            Dir.entries.map { p + it }.filter { (r, c) -> array[r.mod(size)][c.mod(size)] }
-        }.toSet() - prev
+    val array45 = Array(size * 2) { BooleanArray(size * 2) }
+    array45.indices.forEach { r45 ->
+        array45[r45].indices.forEach { c45 ->
+            if (r45 % 2 == c45 % 2) array45[r45][c45] =
+                input[((r45 + c45 + size) / 2).mod(size)][((r45 - c45 + size) / 2).mod(size)] != '#'
+        }
     }
 
-    array.indices.forEach { r ->
-        println(array[r].indices.map { c -> if (Pos(r, c) in prev) 'O' else if (array[r][c]) '.' else '#' }
-            .joinToString(""))
-    }
-    return prev.size.toLong()
+    val p = size - steps..size + steps step 2
+    return p.sumOf { r -> p.count { c -> array45[r.mod(size * 2)][c.mod(size * 2)] } }.toLong()
+
 }
+
--- test/aoc2023/day21/Day21Test.kt
+++ test/aoc2023/day21/Day21Test.kt
@@ -1,11 +1,10 @@
 package aoc2023.day21
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
-import java.util.concurrent.TimeUnit
+import kotlin.test.Test
 import kotlin.test.expect
 
-@Timeout(1, unit = TimeUnit.SECONDS)
+@Timeout(1, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
 class Day21Test {
 
     val testInput: Input? = null
@@ -25,26 +24,23 @@ class Day21Test {
     """.trimIndent()
 
     @Test
-    @Timeout(5)
     fun test6() {
         expect(16L) { calc(parse(testInputStr),6) }
     }
 
     @Test
-    @Timeout(5)
     fun test10() {
         expect(50L) { calc(parse(testInputStr),10) }
     }
 
     @Test
-    @Timeout(5)
     fun test50() {
         expect(1594L) { calc(parse(testInputStr),50) }
     }
 
     @Test
     fun test5000() {
-//        expect(16733044L) { calc(parse(testInputStr),5000) }
+        expect(16733044L) { calc(parse(testInputStr),5000) }
     }
 
 }
