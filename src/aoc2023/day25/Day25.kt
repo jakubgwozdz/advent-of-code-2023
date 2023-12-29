0f89d49165c6f12826f3c3ecb1fe4b8dc2ae9a01
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2023-12-29 07:44:26 +0100

    day 25 correctly

--- src/aoc2023/day25/Day25.kt
+++ src/aoc2023/day25/Day25.kt
@@ -3,10 +3,7 @@ package aoc2023.day25
 import aoc2023.Puzzle
 import aoc2023.expect
 import aoc2023.getDay
-import aoc2023.logged
-import aoc2023.reachable
 import aoc2023.readAndParse
-import kotlin.random.Random
 
 fun main() {
     val input = readAndParse("local/${getDay {}}_input.txt", ::parse)
@@ -15,7 +12,6 @@ fun main() {
     puzzle.part2()
 }
 
-//data class Input(val todo: Int)
 typealias Input = Map<String, List<String>>
 
 fun parse(inputStr: String): Input = buildMap<String, MutableList<String>> {
@@ -27,37 +23,15 @@ fun parse(inputStr: String): Input = buildMap<String, MutableList<String>> {
         }
     }
 }.mapValues { (_, v) -> v.toList() }
-//    .also { it.tgf("local/day25.tgf") }
 
+fun Pair<String, String>.sorted() = if (first <= second) this else second to first
+
+// not 100% correct, but works for my input
 fun part1(input: Input): Int {
-    val nodes = input.keys.toList()
-    val edges = input.flatMap { (k, l) -> l.map { if (k < it) k to it else it to k } }.toSet()
-
-    fun krager(): Set<Pair<String, String>> {
-        val random = Random(0)
-        // Krager's algorithm
-        while (true) {
-            val labeledEdges = edges.associateWith { mutableListOf(it) }.toMutableMap()
-            while (labeledEdges.size > 2) {
-                val (v1, v2) = labeledEdges.keys.random(random)
-                val contracted = labeledEdges.remove(v1 to v2)!!
-
-            }
-            labeledEdges.forEach { it.logged() }
-        }
-    }
-
-    // hack using yEd
-    val cut = setOf("xbl" to "qqh", "tbq" to "qfj", "xzn" to "dsr")
-
-    val reachable = input.mapValues { (k, v) ->
-        val o = cut.singleOrNull { it.first == k }?.second
-            ?: cut.singleOrNull { it.second == k }?.first
-        if (o != null) v - o
-        else v
-    }
-        .reachable("xbl")
-    return reachable.size * (input.size - reachable.size)
+    val subgraph = input.keys.toMutableSet()
+    fun count(v: String) = input[v]!!.count { it !in subgraph }
+    while (subgraph.sumOf(::count) != 3) subgraph.remove(subgraph.maxBy(::count))
+    return subgraph.size * (input.size - subgraph.size)
 }
 
 fun part2(input: Input): Any? = null // TODO("part 2 with ${input.toString().length} data")
